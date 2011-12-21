1fc011fc53b02cd508d5f8827c3fd6c5ebdfcccf
Gun.io Whitespace Robot <contact@gun.io>
2011-12-21 08:20:34 -0500

    Remove whitespace [Gun.io WhitespaceBot]

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 class Image < Asset
-  has_attached_file :attachment, 
-                    :styles => { :mini => '48x48>', :small => '100x100>', :product => '240x240>', :large => '600x600>' }, 
+  has_attached_file :attachment,
+                    :styles => { :mini => '48x48>', :small => '100x100>', :product => '240x240>', :large => '600x600>' },
                     :default_style => :product,
                     :path => "assets/products/:id/:style/:basename.:extension",
                     :storage => "s3",
@@ -15,7 +15,7 @@ class Image < Asset
   # we need to look at the write-queue for images which have not been saved yet
   after_post_process :find_dimensions
   def find_dimensions
-    temporary = attachment.queued_for_write[:original] 
+    temporary = attachment.queued_for_write[:original]
     filename = temporary.path unless temporary.nil?
     filename = attachment.path if filename.blank?
     geometry = Paperclip::Geometry.from_file(filename)
@@ -27,7 +27,7 @@ class Image < Asset
   def validate
     unless attachment.errors.empty?
       # uncomment this to get rid of the less-than-useful interrim messages
-      # errors.clear 
+      # errors.clear
       errors.add :attachment, "Paperclip returned errors for file '#{attachment_file_name}' - check ImageMagick installation or image source file."
       false
     end
